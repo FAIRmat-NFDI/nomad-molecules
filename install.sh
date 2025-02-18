ef61fb9f3de64ebe33b2ef8779be459eaf2af9bf
tdenell <denell@physik.hu-berlin.de>
2025-02-18 15:07:02 +0100

    updated install.sh, removed sudo

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e  # Exit on error
 SCRIPT_DIR=$(dirname "$(realpath "$0")")  # Get the directory of the script
 
 echo "Updating package list..."
-sudo apt update
+apt update
 
 echo "Installing required system dependencies..."
-sudo apt install -y cmake g++ libeigen3-dev libboost-all-dev libxml2-dev libz-dev swig python3-dev python3-pip git
+apt install -y cmake g++ libeigen3-dev libboost-all-dev libxml2-dev libz-dev swig python3-dev python3-pip git
 
 echo "Installing Open Babel..."
 git clone https://github.com/openbabel/openbabel.git
@@ -15,16 +15,16 @@ cd openbabel
 mkdir build && cd build
 cmake .. -DPYTHON_BINDINGS=ON -DPYTHON_EXECUTABLE=$(which python3) -DRUN_SWIG=ON
 make -j$(nproc)
-sudo make install  # Ensure system-wide installation
+make install  # Ensure system-wide installation
 
 cd "$SCRIPT_DIR"
 
 echo "Setting up Open Babel environment..."
-echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf.d/openbabel.conf
-sudo ldconfig
+echo "/usr/local/lib" | tee -a /etc/ld.so.conf.d/openbabel.conf
+ldconfig
 echo 'export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
 source ~/.bashrc
-sudo ldconfig
+ldconfig
 
 echo "Verifying Open Babel installation..."
 obabel -H || { echo "Open Babel installation failed"; exit 1; }
